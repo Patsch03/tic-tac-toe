db4d8cc5cb51f074c22987aff0dd5299fc656092
Patsch03 <pschenk0304@gmail.com>
2022-07-30 05:12:09 -0400

    added check tie game logic

--- javascript/script.js
+++ javascript/script.js
@@ -19,8 +19,20 @@ let boxClickCol1  = document.getElementsByClassName("boxesCol1");
 let boxClickCol2 = document.getElementsByClassName("boxesCol2");
 let boxClickCol3 = document.getElementsByClassName("boxesCol3");
 
+function isBoardFull(){
+    let isBoardFull = true;
+    for(let i = 0; i < gameBoard.length; i++){
+        for(let j = 0; j <gameBoard[i].length; j++){
+            if(gameBoard[i][j] == 0){
+                isBoardFull = false;
+            }
+        }
+    }
+    return isBoardFull;
+}
+// logic for checking if the game is won
+// need to replace the alert with reseting the game board most likely going to add a reset function that happens when button is clicked 
 
-//checking if the game is won
 function checkWin(){
     if(gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0] && gameBoard[0][0] != 0){
         alert("Game is over!");
@@ -38,10 +50,16 @@ function checkWin(){
         alert("Game is over!");
     }else if(gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] && gameBoard[0][2] != 0){
         alert("Game is over!");
+    }else if(isBoardFull()){
+        alert("Game has resulted in a tie");
+
     }
 
 }
 //end of checking if game is won
+
+
+
 
 function gameRun(){
     for (let i = 0; i < boxClickCol1.length; i++) {
