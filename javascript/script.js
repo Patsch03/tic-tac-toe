566faa3c250e21daca56990a55422aadb60e2d63
Patsch03 <pschenk0304@gmail.com>
2022-07-30 18:14:49 -0400

    fully funcional backend tictactoegame

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,19 @@
 let playerTurn = 1;
 
+
 let gameBoard = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
 ];
 
-
-
-
-
-
-
-
-//start of logic to change array on click
+function addXorO(boardPosition){ // 2d arrray position input
+    if(playerTurn == 1){
+        //add X
+    }else if(playerTurn == 2){
+        //add O
+    }
+}
 
 let boxClickCol1  = document.getElementsByClassName("boxesCol1");
 let boxClickCol2 = document.getElementsByClassName("boxesCol2");
@@ -30,28 +30,60 @@ function isBoardFull(){
     }
     return isBoardFull;
 }
+
+
+function resetGame(){
+    gameBoard = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+
+    playerTurn = 1;
+}
+
+
+function whoWon(position){
+    if(position == 1){
+        alert("X's won");
+
+    }else if(position == 2){
+        alert("O's won");
+    }
+
+}
+
 // logic for checking if the game is won
 // need to replace the alert with reseting the game board most likely going to add a reset function that happens when button is clicked 
 
 function checkWin(){
     if(gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0] && gameBoard[0][0] != 0){
         alert("Game is over!");
+        whoWon(gameBoard[0][0]);
     }else if(gameBoard[0][1] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][1] && gameBoard[0][1] != 0){
         alert("Game is over!");
+        whoWon(gameBoard[0][1]);
     }else if(gameBoard[0][2] == gameBoard[1][2] && gameBoard[1][2] == gameBoard[2][2] && gameBoard[0][2] != 0){
         alert("Game is over!");
+        whoWon(gameBoard[0][2]);
     }else if(gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2] && gameBoard[0][0] != 0){
         alert("Game is over!");
+        whoWon(gameBoard[0][0]);
     }else if(gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2] && gameBoard[1][0] != 0){
         alert("Game is over!");
+        whoWon(gameBoard[1][0]);
     }else if(gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2] && gameBoard[2][0] != 0){
         alert("Game is over!");
+        whoWon(gameBoard[2][0]);
     }else if(gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] && gameBoard[0][0] != 0){
         alert("Game is over!");
+        whoWon(gameBoard[0][0]);
     }else if(gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2] && gameBoard[0][2] != 0){
         alert("Game is over!");
+        whoWon(gameBoard[0][2]);
     }else if(isBoardFull()){
         alert("Game has resulted in a tie");
+        
 
     }
 
@@ -62,76 +94,93 @@ function checkWin(){
 
 
 function gameRun(){
-    for (let i = 0; i < boxClickCol1.length; i++) {
-        boxClickCol1[i].addEventListener("click", function () {
-            if(gameBoard[i][0] == 0){
-                if(playerTurn == 1){
-                    gameBoard[i][0] = 1;
-                    console.log(gameBoard);
-                    playerTurn = 2;
-                    checkWin();
-                }else if(playerTurn == 2){
-                    gameBoard[i][0] = 2;
-                    console.log(gameBoard);
-                    playerTurn = 1;
-                    checkWin();
-                }
-            }else{
-                console.log("There is already a value here");
-            }
-            
-            
-        });
-    }
     
-    for (let i = 0; i < boxClickCol2.length; i++) {
-        boxClickCol2[i].addEventListener("click", function () {
-            if(gameBoard[i][1] == 0){
-                if(playerTurn == 1){
-                    gameBoard[i][1] = 1;
-                    console.log(gameBoard);
-                    playerTurn = 2;
-                    checkWin();
-                }else if(playerTurn == 2){
-                    gameBoard[i][1] = 2;
-                    console.log(gameBoard);
-                    playerTurn = 1;
-                    checkWin();
-                }
-            }else{
-                console.log("There is already a value here");
-            }
-            
-            
-        });
-    }
     
-    for (let i = 0; i < boxClickCol3.length; i++) {
-        boxClickCol3[i].addEventListener("click", function () {
-            if(gameBoard[i][2] == 0){
-                if(playerTurn == 1){
-                    gameBoard[i][2] = 1;
-                    console.log(gameBoard);
-                    playerTurn = 2;
-                    checkWin();
-                }else if(playerTurn == 2){
-                    gameBoard[i][2] = 2; 
-                    console.log(gameBoard);
-                    playerTurn = 1;
-                    checkWin();
+        for (let i = 0; i < boxClickCol1.length; i++) {
+            boxClickCol1[i].addEventListener("click", function () {
+                if(gameBoard[i][0] == 0){
+                    if(playerTurn == 1){
+                        gameBoard[i][0] = 1;
+                        console.log(gameBoard);
+                        playerTurn = 2;
+                        checkWin();
+                    }else if(playerTurn == 2){
+                        gameBoard[i][0] = 2;
+                        console.log(gameBoard);
+                        playerTurn = 1;
+                        checkWin();
+                    }
+                }else{
+                    console.log("There is already a value here");
                 }
-            }else{
-                console.log("There is already a value here");
-            }
-            
-            
-        });
+                
+                
+            });
+        }
+        
+        for (let i = 0; i < boxClickCol2.length; i++) {
+            boxClickCol2[i].addEventListener("click", function () {
+                if(gameBoard[i][1] == 0){
+                    if(playerTurn == 1){
+                        gameBoard[i][1] = 1;
+                        console.log(gameBoard);
+                        playerTurn = 2;
+                        checkWin();
+                    }else if(playerTurn == 2){
+                        gameBoard[i][1] = 2;
+                        console.log(gameBoard);
+                        playerTurn = 1;
+                        checkWin();
+                    }
+                }else{
+                    console.log("There is already a value here");
+                }
+                
+                
+            });
+        }
+        
+        for (let i = 0; i < boxClickCol3.length; i++) {
+            boxClickCol3[i].addEventListener("click", function () {
+                if(gameBoard[i][2] == 0){
+                    if(playerTurn == 1){
+                        gameBoard[i][2] = 1;
+                        console.log(gameBoard);
+                        playerTurn = 2;
+                        checkWin();
+                    }else if(playerTurn == 2){
+                        gameBoard[i][2] = 2; 
+                        console.log(gameBoard);
+                        playerTurn = 1;
+                        checkWin();
+                    }
+                }else{
+                    console.log("There is already a value here");
+                }
+                
+                
+            });
+        }
+
     }
 
-}
+
+   
+
+    let resetButton = document.getElementById("reset");
+    resetButton.addEventListener("click", function(){
+        resetGame();
+        alert("Game has been reset");
+    });
+
+
+
 
 gameRun();
 
 
-// end of logic for changing array on click
+
+
+
+
 
